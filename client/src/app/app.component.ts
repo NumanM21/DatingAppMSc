941c6b4e11140177c8bd85040592caea701d2f0a
Numan Maarij <numanmaarij21@gmail.com>
2023-06-06 18:30:55 +0100

    Bootstrap + HTTP Urls

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'Dating app';
+  users: any; 
+
+
+  constructor(private http: HttpClient) {}
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/users').subscribe({
+      next: response => this.users = response,
+      error: error => console.log(error),
+      complete: () => console.log('Request has completed')
+    })
+  }
+// () (we used 'response') represents what we return from the .subscribe (so what we observe)
+// then use a LAMBDA expression => to tell our code what we want to do with this //data
+
+}
